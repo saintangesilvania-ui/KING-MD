1a6e402913ceb7d61ec11967415bbe53b0c7b76d
KING-MD <saintangesilvania@gmail.com>
2026-08-10 00:18:13 -0400

    Add files via upload

---
+++ commands/vv.js
@@ -0,0 +1,37 @@
+ {
+  if (!m.quoted) {
+    return reply(*ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ, ᴠɪᴅᴇᴏ, ᴏʀ ᴀᴜᴅɪᴏ ᴡɪᴛʜ ᴛʜᴇ ᴄᴀᴘᴛɪᴏɴ ${prefix + command}*)
+  }
+  
+  let mime = (m.quoted.msg  m.quoted).mimetype  ''
+  try {
+    if (/image/.test(mime)) {
+      let media = await m.quoted.download()
+      await bad.sendMessage(m.sender, {
+        image: media,
+        caption: "✅ ᴠɪᴇᴡ ᴏɴᴄᴇ ɪᴍᴀɢᴇ sᴇɴᴛ ᴛᴏ ʏᴏᴜʀ ᴅᴍ",
+      }, { quoted: m })
+      
+    } else if (/video/.test(mime)) {
+      let media = await m.quoted.download()
+      await bad.sendMessage(m.sender, {
+        video: media,
+        caption: "✅ ᴠɪᴇᴡ ᴏɴᴄᴇ ᴠɪᴅᴇᴏ sᴇɴᴛ ᴛᴏ ʏᴏᴜʀ ᴅᴍ",
+      }, { quoted: m })
+      
+    } else if (/audio/.test(mime)) {
+      let media = await m.quoted.download()
+      await bad.sendMessage(m.sender, {
+        audio: media,
+        mimetype: 'audio/mpeg',
+        ptt: true
+      }, { quoted: m })
+      
+    } else {
+      reply(❌ ᴜɴsᴜᴘᴘᴏʀᴛᴇᴅ ᴍᴇᴅɪᴀ ᴛʏᴘᴇ!\nʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ, ᴠɪᴅᴇᴏ, ᴏʀ ᴀᴜᴅɪᴏ ᴡɪᴛʜ *${prefix + command}*)
+    }
+  } catch (err) {
+    console.error('ᴇʀʀᴏʀ ᴘʀᴏᴄᴇssɪɴɢ ᴍᴇᴅɪᴀ:', err)
+    reply(ғᴀɪʟᴇᴅ ᴛᴏ ᴘʀᴏᴄᴇss ᴍᴇᴅɪᴀ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ.)
+  }
+}
